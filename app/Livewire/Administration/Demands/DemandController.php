741545883217830d8542bf1ff3dc19f5013347ff
hugoCanul <hcanul@avantronics.com.mx>
2025-02-04 13:22:04 -0500

    Se realizaron cambios de creacion de formulario

---
+++ app/Livewire/Administration/Demands/DemandController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Administration\Demands;
+
+use Livewire\Component;
+
+class DemandController extends Component
+{
+    public function render()
+    {
+        return view('livewire.administration.demands.demand-controller');
+    }
+}
